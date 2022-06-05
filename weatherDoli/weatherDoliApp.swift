4cb3489f1ffa96f3ef81b8937f7d31adc89631ad
dolinkue <dolinkue_n@me.com>
2022-06-05 10:17:22 -0300

    Initial Commit

---
+++ weatherDoli/weatherDoliApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherDoliApp.swift
+//  weatherDoli
+//
+//  Created by Nicolas Dolinkue on 05/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct weatherDoliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
